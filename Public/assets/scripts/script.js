cdb40ac7d482fc63faa814795a301065dc446b7e
fady gamil <fadygamilha@gmail.com>
2023-12-05 02:42:05 +0200

    add T&C pop up

--- Public/assets/scripts/script.js
+++ Public/assets/scripts/script.js
@@ -1,7 +1,13 @@
 const scripturl = 'https://script.google.com/macros/s/AKfycbxUm_XNSJwTurMt6z5jSP6qy_6kOBnAiVCz5NtVLeXxpiR2Y6461Wnbm45kPBuRuu2zEw/exec'
 const msgbox = document.getElementById('msgmsg')
 const form = document.forms['msgform']
-
+const tcpopup = document.getElementById('t_c_popup');
+function openpopup(){
+    tcpopup.style.left="50%";
+}
+function closepopup(){
+    tcpopup.style.left="200%";
+}
 form.addEventListener('submit', e=>{
     e.preventDefault()
     fetch(scripturl , {method:'POST', body: new FormData(form)})
